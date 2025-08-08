203b44afd72ecbce4c266c50dc1f23dafa494dc9
Sparsh12342 <158298418+Sparsh12342@users.noreply.github.com>
2025-08-07 23:48:36 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,14 @@
-const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { BrowserWindow } = require('electron');
 
-const isDev = !app.isPackaged;
-
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 1000,
-    height: 700,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true,
-      sandbox: false,
-      nodeIntegration: false,
-      webSecurity: !isDev
-    }
-  });
-
-  if (isDev) {
-    win.loadURL('http://localhost:5173').catch((err) => {
-      console.error('Failed to load Vite dev server:', err);
-      win.loadFile(path.join(__dirname, '../react-app/dist/index.html'));
-    });
-  } else {
-    win.loadFile(path.join(__dirname, '../react-app/dist/index.html'));
+const mainWindow = new BrowserWindow({
+  width: 800,
+  height: 600,
+  webPreferences: {
+    nodeIntegration: false,  // Usually false for security
+    contextIsolation: true,
   }
-}
-
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
+// Assuming main.js is in /electron-app and React build is in ../react-app/dist
+mainWindow.loadFile(path.join(__dirname, '../react-app/dist/index.html'));
